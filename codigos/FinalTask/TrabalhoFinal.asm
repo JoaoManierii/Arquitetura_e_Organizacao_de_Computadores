4eeba3753beb4e5becdd46cc37ddea233978805a
Joao Otavio Manieri <jotamanieri@ufu.br>
2022-08-05 09:35:31 -0300

    add finalTask

---
+++ codigos/FinalTask/TrabalhoFinal.asm
@@ -0,0 +1,44 @@
+.data
+
+	str2: .asciiz "   "
+	
+	
+.text 
+	add $s0, $zero, $zero 
+	
+	addi	$s3, $zero, 0	
+	addi	$s1, $zero, 1	
+	addi	$s2, $zero, 1	
+	
+FOR1:
+	slti $t1, $s0, 20 
+	beq $t1, $zero, SAI1
+	
+	addi	$v0, $zero, 4
+	la	$a0, str2
+	syscall
+	
+	li $v0, 1
+	add $a0, $s2, $zero
+	syscall
+	
+	add $s2, $s1, $s3 
+		
+	add $s3, $zero, $s1 
+		
+	add $s1, $zero, $s2   
+	
+	addi	$s0, $s0, 1
+	j FOR1
+	
+SAI1:
+	addi	$v0, $zero, 10
+	syscall
+	
+	
+	
+	
+	
+	
+	
+	
